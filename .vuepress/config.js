44b68d70b8013a487445a1c77a01f528df175a5c
Thomas Payet <thomas@meilisearch.com>
2020-06-10 15:23:06 +0200

    Add netlify cms

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -283,5 +283,24 @@ module.exports = {
     fathom('trackPageview');
     `,
     ],
+    [
+      'script',
+      { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }
+    ],
+    [
+      'script',
+      {},
+      `
+        if (window.netlifyIdentity) {
+          window.netlifyIdentity.on("init", user => {
+            if (!user) {
+              window.netlifyIdentity.on("login", () => {
+                document.location.href = "/admin/";
+              });
+            }
+          });
+        }
+      `
+    ]
   ],
 }
